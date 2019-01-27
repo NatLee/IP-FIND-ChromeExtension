a958fb115306efac390f1fa48c6e0c875738c8f6
NatLee <vip3268@gmail.com>
2019-01-28 03:16:58 +0800

    Update

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $.getJSON("https://jsonip.com/?callback=?", function (data) {
         console.log(data);
         $('body').append('<font size="3"><center>Your IP</center></font><br>');
-        $('body').append('<font size="5"><center><b>' + data.ip + '</b></center></font>');
+        $('body').append('<font size="5"><center><b id="myIP">' + data.ip + '</b></center></font>');
         $('body').append('<font size="4"><center> <button class="noSelect" onclick="copyIP(this)">複製IP</button></center></font>');
 
         //alert(data.ip);
@@ -10,23 +10,14 @@ $(document).ready(function () {
 
 });
 
-
-
-function copyIP(e) {
-    try {
-
-        var link = document.querySelector("b");
-        var range = document.createRange();
-        range.selectNode(link);
-
-        window.getSelection().addRange(range);
-        successful = document.execCommand("copy");
-        window.getSelection().removeAllRanges();
-        alert("已複製");
-        
-
-    } catch (err) {
-        alert("抱歉！似乎是哪裡出錯了");
+function copyIP() {
+    try{
+        var copyText = document.getElementById("myIP");
+        copyText.select();
+        document.execCommand("copy");
+        alert("IP Copied: " + copyText.value);
+    } catch(err){
+        alert("Oh! Something wrong!");
     }
-   
 }
+
