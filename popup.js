f407fc6a79cd73df2f693d596124c7a813cae125
NatLee <vip3268@gmail.com>
2019-01-28 06:49:43 +0800

    Fix copy bug and add ip search.

--- popup.js
+++ popup.js
@@ -1,23 +1,79 @@
+
+$.ajaxSettings.async = false;
+
 $(document).ready(function () {
-    $.getJSON("https://jsonip.com/?callback=?", function (data) {
-        console.log(data);
-        $('body').append('<font size="3"><center>Your IP</center></font><br>');
-        $('body').append('<font size="5"><center><b id="myIP">' + data.ip + '</b></center></font>');
-        $('body').append('<font size="4"><center> <button class="noSelect" onclick="copyIP(this)">複製IP</button></center></font>');
-
-        //alert(data.ip);
-    });
-
+    // Find IP
+    $.getJSON('https://json.geoiplookup.io/', function (data) {
+        $('#myIP').text(data.ip);
+    })
+    
 });
 
-function copyIP() {
-    try{
-        var copyText = document.getElementById("myIP");
-        copyText.select();
-        document.execCommand("copy");
-        alert("IP Copied: " + copyText.value);
-    } catch(err){
-        alert("Oh! Something wrong!");
+document.getElementById('copyBtn').onclick = function () {
+    var ip = document.getElementById('myIP');
+
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = ip.innerText;
+    document.body.appendChild(copyFrom);
+    copyFrom.focus();
+    document.execCommand('SelectAll');
+    document.execCommand('Copy');
+    clearSelection()
+    document.body.removeChild(copyFrom);
+    Swal.fire({
+        type: 'success',
+        title: 'Copied',
+        showConfirmButton: false,
+        timer: 500
+    })
+}
+
+
+document.getElementById('searchBtn').onclick = function () {
+    var ip = document.getElementById('searchIP').value.trim();
+    
+    if (ValidateIPaddress(ip)) {
+        reqUrl = 'https://json.geoiplookup.io/?ip=' + ip;
+        $.getJSON(reqUrl, function (data) {
+            $('#searchIpRegion').text('Country: ' + data.region)
+            $('#searchIpCity').text('City: ' + data.district)
+        });
+
     }
+
+}
+
+
+// ref: https://stackoverflow.com/questions/6562727/is-there-a-function-to-deselect-all-text-using-javascript
+function clearSelection() {
+    var sel;
+    if ((sel = document.selection) && sel.empty) {
+        sel.empty();
+    } else {
+        if (window.getSelection) {
+            window.getSelection().removeAllRanges();
+        }
+        var activeEl = document.activeElement;
+        if (activeEl) {
+            var tagName = activeEl.nodeName.toLowerCase();
+            if (tagName == "textarea" ||
+                (tagName == "input" && activeEl.type == "text")) {
+                // Collapse the selection to the end
+                activeEl.selectionStart = activeEl.selectionEnd;
+            }
+        }
+    }
+}
+
+// ref: https://www.w3resource.com/javascript/form/ip-address-validation.php
+function ValidateIPaddress(ipaddress) {
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
+        return (true);
+    }
+    Swal.fire({
+        type: 'error',
+        text: 'IP format wrong!',
+    })
+    return (false);
 }
 
